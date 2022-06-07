86f5b00b42fe23a8952de1d944f194ad8961e746
MalgorzataIzdebska <m.izdebska1998@gmail.com>
2022-06-07 22:36:08 +0200

    CSV 3

--- main.py
+++ main.py
@@ -5,52 +5,37 @@ if __name__ == "__main__":
 
 import csv
 import unittest
+from unittest import TestCase
 
-inputfile = str(input())
-
-calls_sum_dict = dict()
-
-with open(inputfile,'r') as fin: 
-    reader = csv.DictReader(fin, delimiter = ",")
-    headers = next(reader)
-    for row in reader:
-        from_subscr = int(row['From'])
-        if from_subscr not in calls_sum_dict:
-            calls_sum_dict[from_subscr] = 0
-        value = int(row['Duration(seconds)'])
-        calls_sum_dict[from_subscr] += value
-
-#print(max(calls_sum_dict.items(), key = lambda x:x[1]))
-
-class menadzerpolaczen:
+class MenadzerPolaczen:
     def __init__(self, filename):
-      self.filename = filename
-      self.data_dict = self.read_data()
-
+        self.filename = filename
+        self.data_dict = self.read_data()
+    
     def read_data(self):
         calls_dict_sum = dict()
+
         with open(self.filename, 'r') as fin:
-          reader = csv.reader(fin, delimiter= ",")
-          headers = next(reader)
+            reader = csv.reader(fin, delimiter= ",")
+            headers = next(reader)
 
-          for row in reader:
-            from_subsr = int(row[0])
-            if from_subsr not in calls_dict_sum:
-              calls_dict_sum[from_subsr] = 0
-            calls_dict_sum[from_subsr] += 1
-        return calls_dict_sum
-
+            for row in reader:
+                from_subsr = int(row[0])
+                if from_subsr not in calls_dict_sum:
+                    calls_dict_sum[from_subsr]=0
+                calls_dict_sum[from_subsr] += 1
+            return calls_dict_sum
+            
     def pobierz_najczesciej_dzwoniacego(self):
-      return max(self.data_dict.items(), key= lambda x: x[1])
+        return max(self.data_dict.items(), key= lambda x:x[1])
 
-class sprawdzdzwoniacegotest(unittest.TestCase):
-  def test_czy_abonent_najczesciej_dzwoniacy_rozpoznany_poprawnie(self):
-    mp = menadzerpolaczen(inputfile)
-    wynik = mp.pobierz_najczesciej_dzwoniacego()
-    self.assertEqual((226,5), wynik)
+class SprawdzDzwoniacegoTest(TestCase):
+    def test_czy_abonent_najczesciej_dzwoniacy_rozpoznany_poprawnie(self):
+        mp = MenadzerPolaczen("phoneCalls.csv")
+        wynik = mp.pobierz_najczesciej_dzwoniacego()
+        self.assertEqual((226,5), wynik)
 
-unittest.main(argv=[''], defaulttest='sprawdzdzwoniacegotest', exit=False)
+unittest.main(argv=[''], defaultTest='SprawdzDzwoniacegoTest', exit=False)
 
-if __name__ == '__main__':
-    print(menadzerpolaczen(inputfile).pobierz_najczesciej_dzwoniacego())
-    pass
+if __name__ == "__main__":
+    print(MenadzerPolaczen(input()).pobierz_najczesciej_dzwoniacego())
